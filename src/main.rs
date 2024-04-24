f572b87927cf8d7cf68fac39d72c3ca6cb05d82f
Mark Kraus <vindevccm@gmail.com>
2024-04-24 17:35:12 -0400

    stage 4

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,37 @@ use std::{
 
 enum HttpMethod {
     Get,
-    Unhandled(String),
+    Unhandled(),
 }
 
 enum HttpVersion {
     V1_1,
-    Unhandled(String),
+    Unhandled(),
 }
 
-struct Request {
-    method: HttpMethod,
-    path: String,
-    version: HttpVersion,
+enum HttpStatus {
+    Ok,
+    NotFound,
+}
+
+impl HttpStatus {
+    fn into_string(&self) -> String {
+        match self {
+            Self::Ok => String::from("200 OK"),
+            Self::NotFound => String::from("404 Not Found"),
+        }
+    }
+}
+
+struct HttpHeaders {
+    content_type: String,
+    content_length: usize,
+}
+
+struct HttpResponse {
+    status: HttpStatus,
+    headers: HttpHeaders,
+    body: String,
 }
 
 fn main() {
@@ -41,37 +60,91 @@ fn handle_stream_connection(mut stream: TcpStream) -> io::Result<()> {
     let mut status_line = String::new();
     my_buf_reader.read_line(&mut status_line)?;
 
-    println!("status_line: {status_line}");
-
     let mut status_line_split = status_line.split_whitespace();
     let method = status_line_split.next().unwrap();
     let path = status_line_split.next().unwrap();
     let version = status_line_split.next().unwrap();
 
-    println!("method: {method}");
-    println!("path: {path}");
-    println!("version: {version}");
-
     let _method = match method {
         "GET" => HttpMethod::Get,
-        _ => HttpMethod::Unhandled(String::from(method)),
+        _ => HttpMethod::Unhandled(),
     };
 
     let _version = match version {
         "HTTP1/1" => HttpVersion::V1_1,
-        _ => HttpVersion::Unhandled(String::from(version)),
+        _ => HttpVersion::Unhandled(),
     };
 
-    let response_status = match path {
-        "/" => "200 OK",
-        _ => "404 Not Found",
+    let mut resources_split = path.split('/');
+    let root_resource = resources_split.nth(1).unwrap();
+    let nested_resources = resources_split.collect::<Vec<&str>>().join("/");
+
+    println!("root_resource: {root_resource}");
+
+    let response = match root_resource {
+        "" => handle_root(),
+        "echo" => handle_echo(&nested_resources),
+        _ => HttpResponse {
+            status: HttpStatus::NotFound,
+            headers: HttpHeaders {
+                content_type: String::from("text/plain"),
+                content_length: 0,
+            },
+            body: String::new(),
+        },
     };
 
-    let response = format!("HTTP/1.1 {response_status}\r\n\r\n");
+    let HttpResponse {
+        status: response_status,
+        headers,
+        body,
+    } = response;
 
-    println!("response: {response}");
+    let mut response = String::from("HTTP/1.1 ");
+
+    response.push_str(&response_status.into_string());
+    response.push_str("\r\n");
+
+    if headers.content_length > 0 {
+        response.push_str(format!("Content-Type: {}", headers.content_type).as_str());
+        response.push_str("\r\n");
+        response.push_str(format!("Content-Length: {}", headers.content_length).as_str());
+        response.push_str("\r\n");
+        response.push_str("\r\n");
+        response.push_str(&body);
+    } else {
+        response.push_str("\r\n");
+    }
+
+    println!();
+    println!("response:");
+    println!("---------------");
+    println!("{response}");
+    println!("---------------");
 
     stream.write_all(response.as_bytes())?;
 
     Ok(())
+}
+
+fn handle_root() -> HttpResponse {
+    HttpResponse {
+        status: HttpStatus::Ok,
+        headers: HttpHeaders {
+            content_type: String::from("text/plain"),
+            content_length: 0,
+        },
+        body: String::new(),
+    }
+}
+
+fn handle_echo(path: &str) -> HttpResponse {
+    HttpResponse {
+        status: HttpStatus::Ok,
+        headers: HttpHeaders {
+            content_type: String::from("text/plain"),
+            content_length: path.len(),
+        },
+        body: String::from(path),
+    }
 }
