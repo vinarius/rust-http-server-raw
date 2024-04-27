236aae17692eb68d08eff15075c5bc5472151da0
Mark Kraus <vindevccm@gmail.com>
2024-04-27 14:54:50 -0400

    foo

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 use std::net::TcpListener;
 
 use crate::handle_stream_connection::handle_stream_connection;
-use crate::models::{Response, ResponseHeaders, Status};
 
 mod handle_stream_connection;
-mod models;
-mod parse_request;
-mod routes;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
